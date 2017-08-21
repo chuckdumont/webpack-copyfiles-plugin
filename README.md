e96e9ffc4a19d74391e5192ded5a6edf4b8b1a32
chuckd <chuckd@us.ibm.com>
2017-08-21 10:40:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# webpack-copyfiles-plugin
+
+Copies files from the source directory to the target directory.  Can optionally clean directories and rename the target directory using a hash of the contents of the directory after copying, and then replace occurrences of a replacement string in the source with the hash value.  
+
+The plugin is multi-compiler aware, so if the same instance of the plugin is used for multiple compiler runs, then the files are copied only once, and cleaned directories are deleted only once.
+
+## Install
+
+```bash
+npm i -D webpack-copyfiles-plugin
+```
+## Usage
+
+```javascript
+// webpack.config.js
+var CopyFilesPlugin = require('webpack-copyfiles-plugin');
+
+module.exports = {
+  // ... snip ...
+  plugins: [
+		new CopyFilesPlugin({
+		  sourceRoot: path.join(__dirname, "sources"),
+		  targetRoot: path.join(__dirname, "dist", "temp"),
+		  files: require('./NonModuleFiles'),
+		  renameTargetDir: true,
+		  dirHashVarName: '__dirhash__',
+		  cleanDirs: [path.join(__dirname, "dist")]
+		});
+  ],
+  // ... snip ...
+}
+```
+## Options
+
+#### sourceRoot
+
+The root directory containing the source files to copy.  This option is required.
+
+#### targetRoot
+
+The target directory to copy the source files to.  This option is required.
+
+#### files
+
+Array of file/directory names to copy.  Directories are specified with a trailing `/`.  Relative path names are relative to `sourceRoot`.
+
+#### renameTargetDir
+
+If true, the last path component of `targetRoot` will be renamed using a hash of the directory's contents.
+
+#### dirHashVarName
+
+If specified and `renameTargetDir` is true, then occurrences of the specified string in the source modules processed by webpack will be replaced with the hash of the directory contents as a string.
+
+#### cleanDirs
+
+Specifies an array of directory names to remove prior to copying the files.  `targetRoot` is always cleaned and therefore does not need to be specified here.
---
+++ index.js
@@ -0,0 +1,162 @@
+/*
+ * (C) Copyright IBM Corp. 2012, 2016 All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* https://github.com/chuckdumont/webpack-copyfiles-plugin */
+
+const hashFiles = require('hash-files');
+const fs  = require('fs-extra');
+const path = require('path');
+const async = require('async');
+const ConstDependency = require("webpack/lib/dependencies/ConstDependency");
+const BasicEvaluatedExpression = require("webpack/lib/BasicEvaluatedExpression");
+
+/*eslint no-shadow: [2, { "allow": ["callback", "err"] }]*/
+module.exports = class CopyFilesPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+  apply(compiler) {
+
+    compiler.plugin(["run", "watch-run"], (compilation__, callback) => {
+      if (!this.promise) {
+        // First thread here gets to do the copy.
+        this.promise = new Promise((resolve, reject) => {
+          async.each((this.options.cleanDirs || []).concat(this.options.targetRoot), (dir, callback) => {
+            fs.remove(dir, (err) => {
+              if (err) {
+                console.error(err);
+              }
+              callback(err);
+            });
+          }, (err) => {
+            if (err) {
+              return reject(err);
+            }
+            async.parallel([
+              (callback) => {
+                // Copy the files
+                console.log("Copying files to " + this.options.targetRoot);
+                async.each(this.options.files, (file, callback) => {
+                  fs.copy(path.resolve(this.options.sourceRoot, file), path.resolve(this.options.targetRoot, file), (err) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                    callback(err);
+                  });
+                }, (err) => {
+                  callback(err);
+                });
+              },
+              // calculate a hash for the files
+              (callback) => {
+                if (!this.options.renameTargetDir) {
+                  return callback();
+                }
+                const files = [];
+                this.options.files.forEach((file) => {
+                  files.push(path.join(this.options.sourceRoot, file));
+                });
+                hashFiles({
+                  algorithm: "sha256",
+                  files: this.options.files.map((file) => {
+                    return path.join(this.options.sourceRoot, file) + (file.endsWith("/") ? "**" : "");
+                  })
+                }, (err, hash) => {
+                  if (err) {
+                    console.error(err);
+                  }
+                  this.filesHash = Buffer.from(hash, "hex").toString('base64').replace(/[/+=]/g, (c) => {
+                    switch (c) {
+                      case '/': return '_';
+                      case '+': return '-';
+                      case '=': return '';
+                    }
+                    return c;
+                  });
+                  console.log("Hash for copied files = " + this.filesHash);
+                  callback(err);
+                });
+              }
+            ], (err) => {
+              if (err) {
+                return reject(err);
+              }
+              if (this.options.renameTargetDir) {
+                // rename the target directory using the hash
+                let newPath = path.resolve(this.options.targetRoot, "..", this.filesHash);
+                fs.remove(newPath, (err) => {
+                  if (err) {
+                    console.error(err);
+                    return reject(err);
+                  }
+                  fs.rename(path.resolve(this.options.targetRoot), path.resolve(newPath), (err) => {
+                    if (err) {
+                      reject.error(err);
+                    } else {
+                      console.log("Renamed " + this.options.targetRoot + " to " + newPath);
+                      resolve();
+                    }
+                  });
+                });
+              } else {
+                resolve();
+              }
+            });
+          });
+        });
+      }
+      // All threads block until the files have been copied.
+      this.promise.then(() => {
+        callback();
+      }, (err) => {
+        callback(err);
+      });
+    });
+
+    if (this.options.renameTargetDir && this.options.dirHashVarName) {
+
+      compiler.plugin("compilation", (compilaton__, data) => {
+        data.normalModuleFactory.plugin("parser", (parser) => {
+          parser.plugin("expression " + this.options.dirHashVarName , (expr) => {
+            // change dirHashVarName expressions in the source to the hash value as a string.
+            const hash = parser.applyPluginsBailResult("evaluate Identifier " + this.options.dirHashVarName, expr).string;
+            const dep = new ConstDependency("\"" + hash + "\"", expr.range);
+            dep.loc = expr.loc;
+            parser.state.current.addDependency(dep);
+            return true;
+          });
+
+          parser.plugin("evaluate typeof " + this.options.dirHashVarName, (expr) => {
+            // implement typeof operator for the expression
+            var result = new BasicEvaluatedExpression().setString("string");
+            if (expr) {
+              result.setRange(expr.range);
+            }
+            return result;
+          });
+
+          parser.plugin("evaluate Identifier " + this.options.dirHashVarName, (expr) => {
+            var result = new BasicEvaluatedExpression().setString(this.filesHash);
+            if (expr) {
+              result.setRange(expr.range);
+            }
+            return result;
+          });
+        });
+      });
+    }
+  }
+};
